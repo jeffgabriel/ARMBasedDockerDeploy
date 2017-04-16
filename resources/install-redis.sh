7ba462739c7c76dcc4167e3bf1a72f365d801e52
Jeff Gabriel <jeffgabriel@outlook.com>
2017-04-16 11:23:17 -0400

    update pwd use

--- resources/install-redis.sh
+++ resources/install-redis.sh
@@ -7,4 +7,4 @@ sudo mv ./logrotation.conf /etc/logrotate.d
 sudo mv ./docker-redis.service /etc/systemd/system
 sudo systemctl enable docker-redis
 
-sudo docker run --name redis -p 6379:6379 -d redis:3.2-alpine
+sudo docker run --name redis -p 6379:6379 -d redis:3.2-alpine --requirepass $1
